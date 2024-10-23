d2b8531a01575d4616b59c8d3ea6721f3aeb4b0f
pikachu0310 <pikachu13711@gmail.com>
2024-10-23 16:18:40 +0900

    :art: lint

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import minimist from 'minimist';
-import dayjs from 'dayjs';
-import 'dayjs/locale/ja.js';
+import minimist from "minimist";
+import dayjs from "dayjs";
+import "dayjs/locale/ja.js";
 
 const args = minimist(process.argv.slice(2));
 const month = args.m ? parseInt(args.m, 10) - 1 : dayjs().month();
@@ -11,21 +11,21 @@ const startOfMonth = dayjs(new Date(year, month, 1));
 const endOfMonth = dayjs(new Date(year, month + 1, 0));
 
 console.log(`      ${month + 1}月 ${year}`);
-console.log('日 月 火 水 木 金 土');
+console.log("日 月 火 水 木 金 土");
 
 let dayOfWeek = startOfMonth.day();
-let calendarOutput = ' '.repeat(dayOfWeek * 3);
+let calendarOutput = " ".repeat(dayOfWeek * 3);
 
 for (let date = 1; date <= endOfMonth.date(); date++) {
-    calendarOutput += String(date).padStart(2, ' ') + ' ';
+  calendarOutput += String(date).padStart(2, " ") + " ";
 
-    if ((dayOfWeek + date) % 7 === 0) {
-        calendarOutput += '\n';
-    }
+  if ((dayOfWeek + date) % 7 === 0) {
+    calendarOutput += "\n";
+  }
 }
 
 if (calendarOutput.trim().length % 21 !== 0) {
-    calendarOutput += '\n';
+  calendarOutput += "\n";
 }
 
 console.log(calendarOutput);
